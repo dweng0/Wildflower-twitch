57751b8c5a459aa10be96ed39bec06646e20c09d
dweng0 <jmartinsmith@outlook.com>
2020-08-16 06:45:05 +0100

    init commit
    Added all the base stuff

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+   rules: [
+     {
+       test: /\.ts?$/,
+       use: 'ts-loader',
+       exclude: /node_modules/,
+     },
+   ],
+ },
+ resolve: {
+   extensions: [ '.ts', '.js' ],
+ },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "Wildflower"
+  })],
+  optimization: {
+    splitChunks:{
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/](babylonjs)[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk:true
+        },
+        core: {
+          test: /[\\/]src[\\/]/,
+          name: 'game',
+          chunks: 'all',
+          maxSize: 1024
+        }
+      }
+    }
+  }
+};
